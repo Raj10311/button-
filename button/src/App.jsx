3c4f063ff7a16e14e99d39ac033e4fecf0ebd1f1
Raj Hemnani <rajhemnani10311@gmail.com>
2025-12-25 21:12:21 +0530

    update changes

--- button/src/App.jsx
+++ button/src/App.jsx
@@ -4,14 +4,25 @@ import './App.css'
 
 function App() {
   const[Color,setColor] =useState("#ff2b05")
+  const[count,setCount] =useState(0)
+  
 function changecolor(){
     setColor("#ff638c");
 
   }
+function counter(){
+  setCount(count+1)
+}
+function Reset(){
+  setCount(0)
+  setColor("#ff2b05")
+}
 
   return (
     <>
-      <button onClick={changecolor} style={{backgroundColor :Color}} >Submit</button>
+      <button onClick={changecolor} style={{backgroundColor :Color}} >Submit </button>
+      <button onClick = {counter} >Button {count}</button>
+      <button onClick = {Reset} >Reset</button>
     </>
   )
 }
