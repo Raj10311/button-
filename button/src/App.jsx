d4010fc4c57d0c9fdd6a0753a4d928f796e5f00a
Raj Hemnani <rajhemnani10311@gmail.com>
2025-12-26 19:37:16 +0530

    update changes

--- button/src/App.jsx
+++ button/src/App.jsx
@@ -1,28 +1,41 @@
 import { useState } from 'react'
 
 import './App.css'
-
+import {data} from './assets/data.js'
 function App() {
-  const[Color,setColor] =useState("#ff2b05")
+  
   const[count,setCount] =useState(0)
+  const[message,setMessage] =useState("")
+  const[index,setIndex] =useState(0)
   
 function changecolor(){
-    setColor("#ff638c");
+    
+    setIndex(count=>(count + 1 )% data.length);
+    
 
   }
 function counter(){
+  if(count<5) 
   setCount(count+1)
+  else{
+    setMessage("Limit reach")
+    setIndex(data.length)
+  }
 }
+
 function Reset(){
   setCount(0)
-  setColor("#ff2b05")
+  setIndex(0)
+
+
 }
 
   return (
     <>
-      <button onClick={changecolor} style={{backgroundColor :Color}} >Submit </button>
+      <button onClick={changecolor} style={{backgroundColor :data[index]}} >Submit </button>
       <button onClick = {counter} >Button {count}</button>
       <button onClick = {Reset} >Reset</button>
+      <p>{message}</p>
     </>
   )
 }
---
+++ button/src/assets/data.js
@@ -0,0 +1,8 @@
+// data.js
+export const data = [
+  "#ff2b05",
+  "#ff638c",
+  "#05ff2b",
+  "#2b05ff",
+  "#f7ff05"
+];
